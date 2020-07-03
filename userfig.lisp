a68441ca4c83e8d80f582d59d92cb619f6217ac0
bnmcgn@gmail.com <bnmcgn@gmail.com>
2020-07-02 20:42:24 -0700

    Rename in gadgets

--- userfig.lisp
+++ userfig.lisp
@@ -54,7 +54,7 @@ store of some sort - perhaps a hash table - so it can't be an arbitrary value.
         while ,src
         do
           (multiple-value-bind (,curr ,rest)
-              (gadgets:divide-after-true #'listp ,src)
+              (gadgets:part-after-true #'listp ,src)
             (setf ,src ,rest)
             (let ((,names (butlast ,curr))
                   (,spec (car (last ,curr))))
